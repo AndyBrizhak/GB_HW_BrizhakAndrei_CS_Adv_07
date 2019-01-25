ee433dfcfd01d4c4a4077eb12fecbc9f8fc634b7
AndyBrizhak <andrewbrizhak@gmail.com>
2019-01-25 11:00:27 +0800

    +EditDepWindow

---
+++ Emp_Dep/EditDepWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Emp_Dep
+{
+    /// <summary>
+    /// Логика взаимодействия для EditDepWindow.xaml
+    /// </summary>
+    public partial class EditDepWindow : Window
+    {
+        public EditDepWindow(int i, Rep dbEmpDep)
+        {
+            InitializeComponent();
+            btn.Click += delegate
+            {
+                dbEmpDep.DbDepartments[i - 1].DepName = txt.Text;
+                this.DialogResult = true;
+            };
+        }
+    }
+}
