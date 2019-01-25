1e8a2b7e1926bc4f5060e620a4f8d0f32457063a
AndyBrizhak <andrewbrizhak@gmail.com>
2019-01-25 10:55:35 +0800

    +Department

---
+++ Emp_Dep/Department.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+
+namespace Emp_Dep
+{
+    public class Department : INotifyPropertyChanged
+    {
+        private int _depId;
+        private string _depName;
+
+        public Department(string _depName, int _depId)
+        {
+            DepId = _depId;
+            DepName = _depName;
+        }
+
+        public int DepId { get => _depId; set => _depId = value; }
+        public string DepName
+        {
+            get => _depName;
+            set
+            {
+                this._depName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.DepName)));
+            }
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public override string ToString()
+        {
+            return $"{this.DepName}";
+        }
+
+    }
+}
