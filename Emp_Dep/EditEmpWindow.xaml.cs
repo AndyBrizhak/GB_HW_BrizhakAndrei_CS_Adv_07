904d0680c558f7e615395325168b34668b68c2f2
AndyBrizhak <andrewbrizhak@gmail.com>
2019-01-25 11:05:37 +0800

    +EditEmpWindow

---
+++ Emp_Dep/EditEmpWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace Emp_Dep
+{
+    /// <summary>
+    /// Логика взаимодействия для EditEmpWindow.xaml
+    /// </summary>
+    public partial class EditEmpWindow : Window
+    {
+        public EditEmpWindow(int i, Rep dbEmpDep)
+        {
+            InitializeComponent();
+            btn.Click += delegate
+            {
+                var selEmp = dbEmpDep.DbEmployees.Single(e => e.GetHashCode() == i);
+                selEmp.FName = txt.Text;
+                //dbEmpDep.DbEmployees[i].FName = txt.Text;
+                this.DialogResult = true;
+            };
+
+        }
+    }
+}
