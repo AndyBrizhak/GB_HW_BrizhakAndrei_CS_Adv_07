e335b257456a63b53a89cbe9af7937fd451a5e10
AndyBrizhak <andrewbrizhak@gmail.com>
2019-01-26 17:33:07 +0800

    + загрузка данных о департаменте в окне EditDepWindow

--- Emp_Dep/EditDepWindow.xaml.cs
+++ Emp_Dep/EditDepWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Data;
 
 namespace Emp_Dep
 {
@@ -7,6 +8,17 @@ namespace Emp_Dep
     /// </summary>
     public partial class EditDepWindow : Window
     {
+        //public DataRow resultRow { get; set; }
+        //public EditWindow(DataRow dataRow)
+        //{
+        //    InitializeComponent();
+        //    resultRow = dataRow;
+        //}
+
+        //private void Window_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    txt.Text = resultRow["NameDep"].ToString();
+        //}
         public EditDepWindow(int i, Rep dbEmpDep)
         {
             InitializeComponent();
